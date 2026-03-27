b937b4eb8b3399e3a63dfd2e7cf5adc852d6000b
igoryano-movingpay <igor.yano@movingpay.com.br>
2026-03-27 17:46:53 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Header } from './components/header';
+import { Post } from './post';
+import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
+
+function App() {
+  return (
+    <div>
+
+      <Header />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Igor Yano"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam asperiores vero ducimus, vitae laudantium consequatur eos id debitis rem sed facere tempora eum consectetur illum provident expedita aspernatur? Deleniti, tempora."
+          />
+          <Post 
+            author="Giovanna dos Santos"
+            content="lorem ipsun"
+          />
+        </main>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default App
